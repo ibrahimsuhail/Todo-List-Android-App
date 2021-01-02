ba162b6f8b0e8f6d258a86ef9f028e998152ddd4
“notibrahimsuhail” <ibrahimsuhail23@gmail.com>
2021-01-02 02:47:32 -0500

    finished todo app

---
+++ app/src/main/java/com/codepath/simpletodo/EditActivity.java
@@ -0,0 +1,22 @@
+package com.codepath.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditActivity extends AppCompatActivity {
+    EditText etItem;
+    Button btnSave;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+        etItem = findViewById(R.id.etItem);
+        btnSave = findViewById(R.id.btnSave);
+
+        getActionBar().setTitle("Edit item");
+
+    }
+}
